a9e0e0408e7a455b7c1139b9444626440736a647
nvminhtam <nvminhtam2201@gmail.com>
2022-11-25 15:32:38 +0700

    QG-6 - Resolve review

--- src/components/Form/LoginWithGoogleButton.tsx
+++ src/components/Form/LoginWithGoogleButton.tsx
@@ -1,31 +1,63 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Button, Alert } from 'react-bootstrap';
 import styles from './Form.module.scss';
-import { useGoogleLogin } from '@react-oauth/google';
+import { CodeResponse, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import config from '../../config';
+import { useMutation } from '@tanstack/react-query';
+import { setItem } from '../../utils/localStorage';
+import Loader from '../Common/Loader/Loader';
 
 const LoginWithGoogleButton = () => {
-  const googleLogin = useGoogleLogin({
-    onSuccess: async codeResponse => {
-      axios.post('http://localhost:5000/auth/login-with-google', {code:codeResponse.code})
-        .then((result) => {
-          /* eslint-disable no-console */
-          console.log(result.data);
-          /* eslint-disable no-console */
-        })
-        .catch((error) => {
-          /* eslint-disable no-console */
-          console.log(error);
-          /* eslint-disable no-console */
-        })
+  const [errMsg, setErrMsg] = React.useState('');
+  const naviagte = useNavigate();
+  const mutation = useMutation({
+    mutationFn: (codeResponse: CodeResponse) =>
+      axios.post(`${config.apiUrl}/auth/login-with-google`, {code:codeResponse.code}),
+    onSuccess: (data) => {
+      const token = data.data?.accessToken;
+      const expiry = data.data?.expiresIn;
+      setItem('h2t_access_token', token, expiry);
+      naviagte('/');
     },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        setErrMsg(
+          error?.response?.data?.message ||
+            'There was a problem with server. Please try again later.'
+        );
+      }
+    },
+  });
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: (codeResponse) => mutation.mutate(codeResponse),
     flow: 'auth-code',
   });
   return (
     <div className={styles.formOAuth}>
       <div className={styles.google}>
-        <Button variant="outline-primary w-100 rounded-0" onClick = { ()=> googleLogin()} > Continue with Google</Button>
+        <Button 
+          variant="outline-primary w-100 rounded-0" 
+          disabled={mutation.isLoading}
+          onClick = { ()=> googleLogin()}>
+          {mutation.isLoading && (
+            <Loader
+              as="span"
+              isFullPage={false}
+              animation="border"
+              size="sm"
+              role="status"
+              className="me-2"
+            />
+          )}
+          Continue with Google
+        </Button>
       </div>
+      <Alert variant="danger" show={mutation.isError}>
+        {errMsg}
+      </Alert>
     </div>
   );
 };
