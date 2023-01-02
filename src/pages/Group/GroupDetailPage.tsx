1ca5c5d8f4957e381040ac3adeb4d16df6874ee2
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2023-01-03 02:11:13 +0700

    QG-11 - Implement remove user from group

--- src/pages/Group/GroupDetailPage.tsx
+++ src/pages/Group/GroupDetailPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppLayout } from 'components/Layouts';
 import axios, { AxiosResponse } from 'axios';
 import config from 'config';
@@ -6,13 +6,14 @@ import { axiosWithToken } from 'utils';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Loader } from 'components/Common';
 import { useParams } from 'react-router-dom';
-import { Stack, Button, Modal, Form } from 'react-bootstrap';
+import { Stack, Button, Modal, Form, ListGroup } from 'react-bootstrap';
 import { useModal } from 'hooks';
 import { toast } from 'react-toastify';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { GroupDetail, UserPreviewWithRoleInGroup } from 'models';
+import { Role } from 'enums';
 
 type NewUsersInGroupInputs = {
   email: string;
@@ -26,8 +27,11 @@ const schema = yup.object().shape({
 });
 
 function GroupDetailPage() {
+  const [selectedUser, setSelectedUser] =
+    useState<UserPreviewWithRoleInGroup>();
   const queryClient = useQueryClient();
-  const { closeModal, openModal, isShowModal } = useModal();
+  const addUserModal = useModal();
+  const kickUserModal = useModal();
   const { groupId } = useParams();
 
   const {
@@ -45,9 +49,9 @@ function GroupDetailPage() {
     await addUserMutation.mutateAsync(data);
   };
 
-  const handleCloseModal = () => {
+  const handleCloseAddUserModal = () => {
     reset();
-    closeModal();
+    addUserModal.closeModal();
   };
 
   const { data, isLoading, isError } = useQuery({
@@ -71,7 +75,7 @@ function GroupDetailPage() {
     onSuccess: (res: AxiosResponse) => {
       toast.success(res.data.message);
       queryClient.invalidateQueries({ queryKey: ['groupDetail'] });
-      handleCloseModal();
+      handleCloseAddUserModal();
     },
     onError: (error) => {
       if (axios.isAxiosError(error) || error instanceof Error) {
@@ -82,8 +86,29 @@ function GroupDetailPage() {
     },
   });
 
-  // eslint-disable-next-line no-console
-  console.log(data);
+  const setUserRoleMutation = useMutation({
+    mutationFn: (data: { role: Role }) =>
+      axiosWithToken.put(`${config.apiUrl}/groups/${groupId}/setUserRole`, {
+        memberId: selectedUser?.userId,
+        role: data.role,
+      }),
+    onSuccess: (res: AxiosResponse) => {
+      toast.success(res.data.message);
+      queryClient.invalidateQueries({ queryKey: ['groupDetail'] });
+      kickUserModal.closeModal();
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error) || error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error(String(error));
+      }
+    },
+  });
+
+  const handleKickUserInGroup = () => {
+    setUserRoleMutation.mutateAsync({ role: Role.KICK_OUT });
+  };
 
   if (isLoading) {
     return (
@@ -104,7 +129,7 @@ function GroupDetailPage() {
         <Stack direction="horizontal" className="justify-content-end">
           <Button
             variant="info"
-            onClick={openModal}
+            onClick={addUserModal.openModal}
             className="mb-4 fw-semibold"
           >
             Add user
@@ -112,7 +137,7 @@ function GroupDetailPage() {
           {/* Check current user role = owner ? */}
           <Button
             variant="danger"
-            onClick={openModal}
+            onClick={kickUserModal.openModal}
             className="mb-4 ms-2 fw-semibold"
           >
             Delete group
@@ -120,28 +145,59 @@ function GroupDetailPage() {
         </Stack>
         <div className="mb-5">
           <h2 className="fs-3">Owner</h2>
-          <hr></hr>
+          <hr className="border-black"></hr>
           <p className="fs-6">{data.group.ownerUser.fullname}</p>
         </div>
         <div className="mb-5">
           <h2 className="fs-3">Co-owners and Members</h2>
-          <hr></hr>
-          {data.group.userInGroups.map(
-            (userInfo: UserPreviewWithRoleInGroup) =>
-              userInfo.role !== 'owner' && (
-                <div key={userInfo.userId}>
-                  <p className="fs-6">{userInfo.user.fullname}</p>
-                </div>
-              )
-          )}
+          <hr className="border-black"></hr>
+          <ListGroup>
+            {data.group.userInGroups.map(
+              (userInfo: UserPreviewWithRoleInGroup) =>
+                userInfo.role !== 'owner' && (
+                  <ListGroup.Item
+                    onClick={() => setSelectedUser(userInfo)}
+                    key={userInfo.userId}
+                    className="bg-secondary d-flex justify-content-between align-items-center border-0 border-bottom border-primary rounded-0 py-3"
+                  >
+                    <p className="fs-6 fw-semibold">{userInfo.user.fullname}</p>
+                    {/* TODO: Check the current user is the owner */}
+                    <div className="d-flex">
+                      <Form.Group className="d-flex align-items-center">
+                        <Form.Label className="mb-0">Role:</Form.Label>
+                        <Form.Select
+                          size="sm"
+                          value={userInfo.role}
+                          aria-label="Group role select"
+                          id={userInfo.userId}
+                          className="ms-2"
+                          // Implement update role
+                        >
+                          <option value={Role.CO_OWNER}>Co-owner</option>
+                          <option value={Role.MEMBER}>Member</option>
+                        </Form.Select>
+                      </Form.Group>
+                      <Button
+                        variant="danger"
+                        size="sm"
+                        className="ms-3"
+                        onClick={() => kickUserModal.openModal()}
+                      >
+                        Kick
+                      </Button>
+                    </div>
+                  </ListGroup.Item>
+                )
+            )}
+          </ListGroup>
         </div>
         {/* TODO: Extract Modal to Common */}
         <Modal
           size="lg"
           aria-labelledby="add-user-group"
           centered
-          show={isShowModal}
-          onHide={handleCloseModal}
+          show={addUserModal.isShowModal}
+          onHide={handleCloseAddUserModal}
         >
           <Modal.Header closeButton>
             <Modal.Title id="add-user-group">Add a user</Modal.Title>
@@ -161,7 +217,7 @@ function GroupDetailPage() {
               </Form.Group>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={handleCloseModal}>
+              <Button variant="secondary" onClick={handleCloseAddUserModal}>
                 Cancel
               </Button>
               <Button
@@ -173,6 +229,32 @@ function GroupDetailPage() {
               </Button>
             </Modal.Footer>
           </Form>
+        </Modal>
+        <Modal
+          size="lg"
+          aria-labelledby="kick-user-group"
+          centered
+          show={kickUserModal.isShowModal}
+          onHide={kickUserModal.closeModal}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="add-user-group">Confirm kick the user</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to kick {selectedUser?.user.fullname}?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={kickUserModal.closeModal}>
+              Cancel
+            </Button>
+            <Button
+              variant="danger"
+              onClick={handleKickUserInGroup}
+              disabled={setUserRoleMutation.isLoading}
+            >
+              {addUserMutation.isLoading ? <Loader size="sm" /> : 'Kick'}
+            </Button>
+          </Modal.Footer>
         </Modal>
       </div>
     </AppLayout>
